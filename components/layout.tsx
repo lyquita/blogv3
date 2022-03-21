5a56ff9f4939c62a10317b215c9741989e8f0b8e
hireoochen@gmail.com <hireoochen@gmail.com>
2022-03-21 13:19:33 +0800

    bug fixs for desktop responsive

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Layout: React.FC<{}> =({children})=>{
 
     return(
-       <div className="px-10 sm:px-80 bg-[#FFFBF0] py-2 text-xs sm:px-5">
+       <div className="px-10 lg:px-80 bg-[#FFFBF0] py-2 text-xs sm:px-5">
            <ul className="flex w-full font-normal nav-list justify-center">
                <li>
                    <Link href="/">Home</Link>
