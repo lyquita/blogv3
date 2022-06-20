e9d964e8b3b0eef97590a41029eec5cdac08b465
hireoochen@gmail.com <hireoochen@gmail.com>
2022-06-20 21:33:38 +0800

    bug fix

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,7 +5,7 @@ const Layout: React.FC<{}> =({children})=>{
   const {systemTheme, theme, setTheme} = useTheme();
 
     return(
-       <div className="px-10 lg:px-80 bg-[#FFFBF0] dark:bg-[#0e0c0c] dark:bg-opacity-90 py-2 text-xs sm:px-5 ">
+       <div className="px-10 lg:px-80 bg-[#FFFBF0] dark:bg-[#0e0c0c] dark:bg-opacity-90 py-2 text-xs sm:px-5 h-screen overflow-scroll">
            <ul className="flex w-full font-normal nav-list justify-center dark:text-[#DCA54C] lg:text-lg">
                <li >
                    <Link href="/">Home</Link>
@@ -14,8 +14,8 @@ const Layout: React.FC<{}> =({children})=>{
                    <Link href="/ideas/">Ideas</Link>
                </li>
                <li className="flex">
-                 <button className="btn btn-xs mr-1" onClick={()=>{setTheme('dark')}}>🌃</button>
-                 <button className="btn btn-xs" onClick={()=>{setTheme('light')}}>☀️</button>
+                 {/* <button className="btn btn-xs mr-1" onClick={()=>{setTheme('dark')}}>🌃</button>
+                 <button className="btn btn-xs" onClick={()=>{setTheme('light')}}>☀️</button> */}
                </li>
            </ul>
            <main>
